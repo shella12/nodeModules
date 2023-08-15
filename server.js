8cecaa2fd112d82dc10101aa8fc666f7ae6881c8
shella12 <ayeshaarshad4567@gmail.com>
2023-08-15 16:10:50 +0500

    Finish express tutorial

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const http = require("http");
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use((req,res,next) => {
-  console.log("mooooo.....")
-  next();
-})
-app.use((req,res,next) => {
-  console.log("second moooo.....")
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+ res.status(404).sendFile(path.join(rootDir,'/views/404.html'));
+});
 
 app.listen(3000);
